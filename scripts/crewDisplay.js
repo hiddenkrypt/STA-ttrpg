2973b26681771d107b9cee37905bef2d2cd3e03c
Ada Clark <hiddenkrypt@gmail.com>
2022-06-10 16:20:59 -0700

    crew display redesign! Data driven approach with SPA influences. Crew records to be displayed soon.

---
+++ scripts/crewDisplay.js
@@ -0,0 +1,88 @@
+//crewDisplay.js
+
+  console.log("load");
+
+function init(){
+  console.log("loaded");
+  let URLParams = {};
+  window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+    URLParams[key] = value;
+  });
+  let listContainer = document.getElementById("crewListContainer");
+  let memberContainer = document.getElementById("crewMemberContainer");
+  let sortMode = "division";
+  const rankOrder = {
+    "Captain": 0, 
+    "Commander": 1, 
+    "Lieutenant Commander": 2,
+    "Lieutenant": 3,
+    "Lieutenant Junior Grade": 4,
+    "Ensign": 5, 
+    "Chief Petty Officer":10 
+  };
+  function displayList(){
+    populateList();
+    memberContainer.style.display = "none";
+    listContainer.style.display = "block";
+  }
+  function displayMember(){
+    listContainer.style.display = "none";
+    memberContainer.style.display = "block";
+  }
+  function populateList(){
+    console.log("sort:"+sortMode);
+    
+    let list = document.getElementById("list");
+    list.innerHTML = "";
+    let groups = crewData.map(e=>e[sortMode]).filter((e,i,a)=>a.indexOf(e)===i);
+    groups = groups.sort();
+    if( sortMode == "rank" ){
+      groups = groups.sort( (a,b) => rankOrder[a] - rankOrder[b] ); 
+    }
+    let buckets = new Array(groups.length).fill(0).map(e=>new Array(0));
+    crewData.forEach( e=> {
+      buckets[ groups.indexOf( e[sortMode] ) ].push( e ) 
+    });
+    buckets.forEach( bucket =>{
+      let bucketContainer = document.createElement("div");
+      let bucketTitle = document.createElement("div");
+      bucketTitle.classList.add("bucketTitle");
+      bucketTitle.innerHTML = bucket[0][sortMode];
+      if(bucketTitle.innerHTML == ""){
+        bucketTitle.innerHTML = "[DATA MISSING]";
+      }
+      bucketContainer.appendChild(bucketTitle);
+      list.appendChild(bucketContainer);
+      let bucketList = document.createElement("ul");
+      bucketList.classList.add("bucketList");
+      bucketContainer.appendChild( bucketList );
+      bucket.forEach( item =>{        
+        let bucketItem = document.createElement("li");
+        bucketItem.classList.add("bucketItem");
+        bucketList.appendChild( bucketItem );
+        bucketItem.innerHTML = item.title;
+      });
+    
+    });
+    
+  }
+  document.getElementById("listControls").addEventListener("click",e=>{
+    document.getElementById("groupDropdown").style.display = "flex";
+    document.getElementById("listControls").style.backgroundColor = "#000";
+  });
+  document.getElementById("groupByRank").addEventListener("click",e=>{setGroup("rank")});
+  document.getElementById("groupByName").addEventListener("click",e=>{setGroup("name")});
+  document.getElementById("groupByPronouns").addEventListener("click",e=>setGroup("pronounsShort"));
+  document.getElementById("groupByDivision").addEventListener("click",e=>setGroup("division"));
+  document.getElementById("groupBySpecies").addEventListener("click",e=>setGroup("species"));
+  function setGroup(mode){
+    document.getElementById("groupDropdown").style.display = "none";
+    document.getElementById("listControls").style.backgroundColor = "#6cf";
+    console.log(mode);
+    sortMode = mode;
+    displayList();
+  }
+  setGroup( "rank" );
+}
+
+window.onload = init;
