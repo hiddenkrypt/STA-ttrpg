30177aa77a36f3d865b19f25ba15e5609ab3e847
hiddenkrypt <hiddenkrypt@gmail.com>
2024-01-31 21:59:02 -0800

    updating data with some Lexington crew

--- crew/crewData.js
+++ crew/crewData.js
@@ -1,5 +1,5 @@
 var crewData = [
-  {
+  {//rala
     "player": "Ada",
     "name": "NFN/NMI Llei'rala",
     "title": "Lt. Cmdr. Dr. Llei'rala  M.D./Ph.D",
@@ -16,7 +16,7 @@ var crewData = [
     "priorHistory": "<h4>Early Life</h4>Llei'rala grew up with her parents, Kenra'sere and Seihwi, on the small Romulan Empire colony of Dier'Dalan. By all accounts she showed an early talent and enthusiasm for medicine. In 2374 the colony was attacked by the dominion. Llei'rala and her mother Kenra'sere escaped and began living on Romulus, but her parent Seiwhi died in the attack.  She completed her initial studies on Romulus at the Medical Academy of the Romulan Empire, and began post-graduate studies at the same until 2387. <br>Llei'rala was evacuated from Romulus before it's destruction from the romulan Supernova in 2387. Her mother died in the event. She spent two years volunteering as a medic to help her fellow refugees, before applying to starfleet with a goal of finishing her MD.</p><p><h4>Starfleet Medical Academy</h4>Llei'rala attended Starfleet Medical Academy in 2390 and earned her Doctorate of Medicine and Philosophy in 2395. However, at the time she was only interested in the medical program, and so was not a starfleet cadet; a rare but not unheard of situation.<br>Llei'rala was a rigorous student, and impressed professors with the knowledge she had already gained through prior study. This did not offer much of a shortcut in her studies, however, as her previous time at the Medical Academy of the Romulan Empire did not include nearly as much Xenobiology as required by Starfleet, and a large amount of her existing studies were not accepted.<h4>Starfleet service and Enlistment</h4>Llei'Rala's residency included a tour as a civilian consultant on a starfleet medical ship, cementing her association with the Federation. At the end of that tour, Llei'rala enrolled in Starfleet Academy as a cadet. She was considered a model student and graduated with honors.</p>",
     "serviceRecord": "<ul><li>2395 - Awarded MD-PhD dual degree.      <li>2395 - Residency at Johns Hopkins Hospital (2 years)<li>2397 - Residency as Corpsman on <i>USS Daishan Dao</i> Hospial Ship (2 years)<li>2399 - Enrolled in Starfleet Academy<li>2402 - Graduated with honors from Starfleet Academy and Commissioned as Lieutenant Junior Grade<li>2402 - Assigned to <i>U.S.S. Phan Xi Păng</i> as Medical Officer (2 years)<ul><li>2403 - Captain's Commendation</ul>     <li>2404 - Assigned to <i>U.S.S. Whitney</i> as Medical Officer (5 years)     	<ul><li>2406 - Captain's Commendation<li>2406 - Promoted to Lieutenant<li>2408 - Awarded Starfleet Silver Palm for actions on stardate 57231<li>2409 - Awarded Extended Tour Ribbon </li>         <li>2409 - Promoted to Lieutenant commander</ul></li><li>2409 - Assigned to Starbase 413 as Research Medical Officer<ul><li>2413 - Received The Machri-Cherk award for outstanding medical research<li>2414 - Awarded 20 Years of Service Ribbon</ul><li>2416 - Assigned to <i>U.S.S. Sierra</i> as Chief Medical Officer</ul>"
   },
-  {
+  {//lok
     "player": "NPC",
     "name": "Lok",
     "title": "Capt. Lok",
@@ -33,7 +33,7 @@ var crewData = [
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//floofles
     "player": "Tracy",
     "name": "Ignatious Floofles",
     "title": "Dr. Floofles",
@@ -41,16 +41,16 @@ var crewData = [
     "pronouns": "It/They",
     "pronounsShort": "It/They",
     "rank": "Commander",
-    "serial": "",
+    "serial": "QET 918-38-279",
     "division": "Science",
-    "assignment": "Councilor",
+    "assignment": "Head Councilor",
     "species": "Tribble Colony",
-    "dob": "",
-    "pob": "",
-    "priorHistory": "",
-    "serviceRecord": ""
+    "dob": "Unknown, discovered in 2300",
+    "pob": "Unknown",
+    "priorHistory": "As far as they know they are unique and no other member of their species currently exists. There is one mention during the TOS era of an encounter on a Starfleet vessel, however in that instance it was due to manipulation of the Tribble genome to try to slow their procreation. They were found by Starfleet around 2300 and were studied in their facilities for more than 60 years, eventually they were able to leave as the scientists agreed that there was no further information that could be gathered in a lab environment. They used this autonomy to attend university on Earth and have multiple degrees in medicine and psychology.",
+    "serviceRecord": "After practicing on Earth for more than two decades, they joined Starfleet around 2395 and have served on various ships since.  While teaching at Johns Hopkins, they first encountered Llei'rala and they became friends."
   },
-  {
+  {//knight
     "player": "NPC",
     "name": "Tobias Knight",
     "title": "Cmdr. Tobias Knight",
@@ -67,7 +67,7 @@ var crewData = [
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//kaleo
     "player": "NPC",
     "name": "Ezra Kaleo",
     "title": "Cmdr. Ezra Kaleo",
@@ -84,7 +84,7 @@ var crewData = [
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//concrex
     "player": "NPC",
     "name": "Azie Concrex",
     "title": "Lt. Cmdr. Azie Concrex",
@@ -101,7 +101,7 @@ var crewData = [
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//katag
     "player": "Mekk",
     "name": "NFN/NMI Katag",
     "title": "Lt. Cmdr. Katag",
@@ -109,7 +109,7 @@ var crewData = [
     "pronouns": "None, He/They if you must",
     "pronounsShort": "None",
     "rank": "Lieutenant Commander",
-    "serial": "",
+    "serial": "N849618366",
     "division": "Science",
     "assignment": "Science Department Head",
     "species": "Tholian",
@@ -118,7 +118,7 @@ var crewData = [
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//pavil
     "player": "Tracy",
     "name": "Pavil Shran",
     "title": "Lt. Cmdr. Pavil Shran",
@@ -126,16 +126,16 @@ var crewData = [
     "pronouns": "They/Them",
     "pronounsShort": "They/Them",
     "rank": "Lieutenant Commander",
-    "serial": "",
+    "serial": "UE-940-991 ROP",
     "division": "Science",
     "assignment": "Head of Astrometrics",
     "species": "Andorian",
-    "dob": "",
-    "pob": "",
-    "priorHistory": "",
+    "dob": "2386",
+    "pob": "Andorian trading vessel",
+    "priorHistory": "They were born in space and spent their formative years visiting multiple other planets. While they do observe some Andorian traditions, they have a much more multicultural heritage due to these experiences and often feel somewhat out of place around other Andorians. Graduated from the Vulcan Science Academy.",
     "serviceRecord": ""
   },
-  {
+  {//bune
     "player": "NPC",
     "name": "NFN/NMI Bune",
     "title": "Lt. Bune",
@@ -152,7 +152,7 @@ var crewData = [
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//kall
     "player": "NPC",
     "name": "Shryreh Kall",
     "title": "Lt. Shryreh Kall",
@@ -169,7 +169,7 @@ var crewData = [
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//tilou
     "player": "Brett",
     "name": "Tilou Metri",
     "title": "Lt. Tilou Metri",
@@ -180,13 +180,13 @@ var crewData = [
     "serial": "",
     "division": "Engineering",
     "assignment": "",
-    "species": "",
-    "dob": "",
-    "pob": "",
+    "species": "Keveli",
+    "dob": "Unknown",
+    "pob": "Unknown",
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//sesh
     "player": "Mekk",
     "name": "Seshana Rell",
     "title": "Lt. Seshana Rell",
@@ -194,16 +194,16 @@ var crewData = [
     "pronouns": "She/Her",
     "pronounsShort": "She/Her",
     "rank": "Lieutenant",
-    "serial": "",
-    "division": "",
+    "serial": "JL-034-268 BNF",
+    "division": "Science",
     "assignment": "",
     "species": "Cardassian",
-    "dob": "",
-    "pob": "",
+    "dob": "2384",
+    "pob": "Masad, Cardassia",
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//tez
     "player": "NPC",
     "name": "Drozada Tez",
     "title": "Lt. Drozada Tez",
@@ -220,7 +220,7 @@ var crewData = [
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//zepht
     "player": "Scott",
     "name": "NFN/NMI Zepht",
     "title": "Lt. Zepht",
@@ -237,7 +237,7 @@ var crewData = [
     "priorHistory": "<h4>Early Life</h4>Zepht was raised on a Denobulan frontier colony to a pair of diplomats who works to coordinate efforts between the various colony settlements and trade with offworld organizations. From a young age Zepht showed a high aptitude for cooperative task, as well as an interest in shuttles and other small flight craft. His early adult life saw him piloting shuttlecraft carrying supplies, dignitaries, and personnel between the various settlements of the colony, and visiting starships.</p><p><h4>Starfleet Academy</h4>Having lived through the Dominion War during early childhood, Zepht felt the need to do their part when war broke out between the Federation and the Klingon Empire. Enlisting in Starfleet in 2408, they attended a flight and repair school located at the Utopia Planetia shipyards, where several professors noted Zepht's adaptive flight skills and ability direct group tasks to completion far ahead of schedule.  They graduated in 2410 and began their service as a pilot and remote repair technician.</p>",
     "serviceRecord": "<ul> <li>2410 - Completed Enlistment Training, designated as a pilot and repair technician. <li>2410 - Assigned to the USS Thunderchild <ul> <li>2415 - Awarded the Decoration for Valor and Gallantry in 2414 for emergency repairs performs during battle. <li>2415 - Commissioned as an officer, Lieutenant Junior Grade and assigned as a fighter squadron commander. <li>2415 - Awarded Silver Palms for action perform during the Iconian War. <li>2415 - Awarded Extended Tour Ribbon <li>2415 - Granted an Extended Leave of Absence. </ul></li> <li>2415 - Received supplementary emergency response training. <li>2415 - Promoted to Lieutenant Senior Grade <li>2416 - Assigned to U.S.S. Sierra as a squadron commander and emergency response specialist. </li> </ul>"
   },
-  {
+  {//zhal
     "player": "Ada",
     "name": "Vela Zhal",
     "title": "Lt.Jg. Vela Zhal",
@@ -254,7 +254,7 @@ var crewData = [
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//velik
     "player": "Brett",
     "name": "NFN/NMI Velik",
     "title": "CPO Velik",
@@ -271,7 +271,7 @@ var crewData = [
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//tlos
     "player": "NPC",
     "name": "NFN/NMI T'los",
     "title": "Ensign T'los",
@@ -288,7 +288,7 @@ var crewData = [
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//jhahr
     "player": "NPC",
     "name": "Zereh Jhahr",
     "title": "Lt. Zereh Jhahr",
@@ -305,7 +305,7 @@ var crewData = [
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//zirva
     "player": "NPC",
     "name": "NFN/NMI Zirva",
     "title": "Lt. Zirva",
@@ -322,7 +322,7 @@ var crewData = [
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//lyalis
     "player": "NPC",
     "name": "Starr Deretha Lyalis",
     "title": "Lt. Starr Deretha Lyalis",
@@ -339,7 +339,7 @@ var crewData = [
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//pid
     "player": "NPC",
     "name": "NFN/NMI Pid",
     "title": "Lt. Pid",
@@ -356,7 +356,7 @@ var crewData = [
     "priorHistory": "",
     "serviceRecord": ""
   },
-  {
+  {//kiisa
     "player": "NPC",
     "name": "NFN/NMI Kiisa",
     "title": "Lt. Kiisa",
@@ -372,7 +372,161 @@ var crewData = [
     "pob": "",
     "priorHistory": "",
     "serviceRecord": ""
+  },
+  {//boq
+    "player": "Ada",
+    "name": "NFN/NMI Boq",
+    "title": "Ensign Boq",
+    "profileImg": "crew/Boq.png",
+    "pronouns": "She/Her",
+    "pronounsShort": "She/Her",
+    "rank": "Ensign",
+    "serial": "22-AL834-95",
+    "division": "Engineering",
+    "assignment": "",
+    "species": "Klingon",
+    "dob": "2392",
+    "pob": "Qo'nos",
+    "priorHistory": "",
+    "serviceRecord": ""
+  },
+  {//aelia
+    "player": "Ada",
+    "name": "NFN/NMI Aelia",
+    "title": "Ensign Aelia",
+    "profileImg": "crew/Aelia.png",
+    "pronouns": "She/Her",
+    "pronounsShort": "She/Her",
+    "rank": "Ensign",
+    "serial": "AA034-06921HBD",
+    "division": "Ops",
+    "assignment": "Helm",
+    "species": "Deltan",
+    "dob": "2249",
+    "pob": "Delta IV",
+    "priorHistory": "Originally served on the USS Lexington during it's disappearance in 2270. It has recently been determined that the crew of that ship was actaully transported forward in time to the year 2416. Now she serves on the USS Sierra as Helm operator.",
+    "serviceRecord": ""
+  },
+  {//r'mierre
+    "player": "Brett",
+    "name": "R'mierre",
+    "title": "Cmdr. R'mierre",
+    "profileImg": "crew/Rmierre.png",
+    "pronouns": "He/Him",
+    "pronounsShort": "He/Him",
+    "rank": "Commander",
+    "serial": "",
+    "division": "Command",
+    "assignment": "XO",
+    "species": "Kaitian",
+    "dob": "",
+    "pob": "",
+    "priorHistory": "",
+    "serviceRecord": ""
+  },
+  {//rosa
+    "player": "Tracy",
+    "name": "Rosa Shran",
+    "title": "Cmdr. Rosa Shran",
+    "profileImg": "crew/Rosa.png",
+    "pronouns": "They/Them",
+    "pronounsShort": "They/Them",
+    "rank": "Commander",
+    "serial": "79031-589770",
+    "division": "Security",
+    "assignment": "Ground Combat Lead, Trainer for close combat/ground combat/small unit tactics",
+    "species": "Andorian",
+    "dob": "2230, Age 40",
+    "pob": "Lursen Prime",
+    "priorHistory": "Fought in all of the major Klingon conflicts starting with the Four Years War through the Federation Klingon War of 2269. They lost their family when the Klingons attacked Lursen Prime and left only a single survivor in approximately 2267.",
+    "serviceRecord": ""
+  },
+  {
+    "player": "Tracy",
+    "name": "Ensign Kai",
+    "title": "Ens NFN/NMI Kai",
+    "profileImg": "crew/Kai.png",
+    "pronouns": "They/Them",
+    "pronounsShort": "They/Them",
+    "rank": "Ensign",
+    "serial": "DIF 863-90-170",
+    "division": "Science",
+    "assignment": "Navigation / Conn",
+    "species": "Pygmy Sperm Whale",
+    "dob": "2406",
+    "pob": "Earth",
+    "priorHistory": "They are 11ft / 400kg in size, social structure is matriarchal and has a multilevel social structure that is based around clans. They are very communist in nature and help and support each other. Neat fact they have an ink sac!",
+    "serviceRecord": ""
+  },
+  {
+    "player": "Mekk",
+    "name": "T'lair",
+    "title": "Ensign NFN/NMI T'lair",
+    "profileImg": "crew/Tlair.png",
+    "pronouns": "",
+    "pronounsShort": "",
+    "rank": "Ensign",
+    "serial": "06927-920440",
+    "division": "Operations",
+    "assignment": "",
+    "species": "Vulcan",
+    "dob": "",
+    "pob": "",
+    "priorHistory": "",
+    "serviceRecord": ""
   }/*,
+  {
+    "player": "",
+    "name": "",
+    "title": "",
+    "profileImg": "crew/",
+    "pronouns": "",
+    "pronounsShort": "",
+    "rank": "",
+    "serial": "",
+    "division": "",
+    "assignment": "",
+    "species": "",
+    "dob": "",
+    "pob": "",
+    "priorHistory": "",
+    "serviceRecord": ""
+  },
+  {
+    "player": "",
+    "name": "",
+    "title": "",
+    "profileImg": "crew/",
+    "pronouns": "",
+    "pronounsShort": "",
+    "rank": "",
+    "serial": "",
+    "division": "",
+    "assignment": "",
+    "species": "",
+    "dob": "",
+    "pob": "",
+    "priorHistory": "",
+    "serviceRecord": ""
+  },
+  {
+    "player": "",
+    "name": "",
+    "title": "",
+    "profileImg": "crew/",
+    "pronouns": "",
+    "pronounsShort": "",
+    "rank": "",
+    "serial": "",
+    "division": "",
+    "assignment": "",
+    "species": "",
+    "dob": "",
+    "pob": "",
+    "priorHistory": "",
+    "serviceRecord": ""
+  },
+  /*,
 
   {
     "player": "",
